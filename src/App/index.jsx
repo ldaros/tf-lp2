2ba0af121394ccf85d5babcf4f3677cdb61ac8fc
ldaros <darosluan@gmail.com>
2021-11-06 12:00:23 -0300

    projeto limpo

---
+++ src/App/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./style.scss";
+
+export default class index extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
