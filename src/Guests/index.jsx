1b439bf6c50ea93e0e00ac3acdccc5461979a298
ldaros <darosluan@gmail.com>
2021-11-08 15:41:02 -0300

    seção convidados mais inteligente

--- src/Guests/index.jsx
+++ src/Guests/index.jsx
@@ -1,52 +1,63 @@
 import React from "react";
-import { Grid, Typography, Box } from "@mui/material"; // Componentes Material UI
-import { cyan } from "@mui/material/colors"; // Cores do Material UI
-import GuestCard from "./GuestCard"; // Componente de Card de convidado
+import { Grid, Typography } from "@mui/material"; // Componentes Material UI
+import { useTheme } from "@mui/material/styles";
 
-// TODO: Responsividade
+import Guest from "./Guest"; // Componente de Card de convidado
 
-// Estilo texto de título
-const headerStyle = {
-  textAlign: "center",
-  color: cyan[900],
-  fontWeight: "bold",
+import imgBueno from "./media/bueno.png"; // Imagem Perfil do Bueno
+import imgTenorio from "./media/tenorio.png"; // Imagem Perfil do Tenorio
+
+const convidados = {
+  tenorio: {
+    nome: "Jeferson Tenório",
+    texto:
+      "Escritor de O beijo na parede, eleito o livro do ano pela Associação Gaúcha de Escritores.",
+    img: imgTenorio,
+  },
+  bueno: {
+    nome: "Eduardo Bueno",
+    texto:
+      "Eduardo Bueno é um jornalista, tradutor, escritor e youtuber no canal Buenas Ideias, onde narra a história do Brasil de forma descontraída.",
+    img: imgBueno,
+  },
 };
 
-// Estilo texto de descrição
-const subHeaderStyle = {
-  textAlign: "center",
-  fontWeight: "normal",
-  color: "#5A5A5A",
-  fontSize: "0.95em",
-};
+export default function Guests() {
+  const theme = useTheme(); // Importação do tema do Material UI
 
-export default function index() {
   return (
-    <Grid container spacing={2} alignItems="center" paddingX={3} paddingY={3}>
+    <Grid
+      container
+      spacing={2}
+      alignItems="stretch"
+      paddingX={{ xs: 3, md: 20 }}
+      paddingY={3}
+    >
       <Grid item xs={12}>
-        {/* Titulo e subtitulo */}
-        <Typography variant="h5" style={headerStyle} mb={2}>
+        <Typography
+          variant="h5"
+          align="center"
+          color={theme.palette.primary.dark}
+          mb={1}
+        >
           Convidados
         </Typography>
-        <Typography style={subHeaderStyle} mb={3}>
+
+        <Typography
+          mb={3}
+          variant="body2"
+          align="center"
+          color={theme.palette.grayText}
+        >
           Confira algums dos autores presentes nesta edição da Feira do Livro:
         </Typography>
       </Grid>
 
-      {/* 
-        xs representa o tamnho de telas pequenas, como por exemplo, tablets e smartphones.
-        xs={12} faz com que o componente ocupe 12 colunas em telas pequenas.
-        md representa o tamanho de telas medias, como por exemplo, desktops.
-        md={6} faz com que o componente passe a ocupar 6 colunas em telas medias.
-      
-        Leia a documentação do Material UI para mais informações sobre os tamanhos de telas: https://mui.com/components/grid/
-      */}
-
       <Grid item xs={12} md={6}>
-        <GuestCard tenorio />
+        <Guest convidado={convidados.tenorio} />
       </Grid>
       <Grid item xs={12} md={6}>
-        <GuestCard bueno />
+        <Guest convidado={convidados.bueno} even />
       </Grid>
     </Grid>
   );
