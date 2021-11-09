08f3546ee14b515fe07d2cf4a28ce52399640a81
ldaros <darosluan@gmail.com>
2021-11-09 19:02:49 -0300

    Codigo ajustado

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -5,35 +5,34 @@ import { cyan, lightGreen } from "@mui/material/colors"; // Cores do material-ui
 
 import Hero from "../Hero";
 import Guests from "../Guests";
-import Localização from "../Localização";
+import Local from "../Local";
 
 // Tema personalizado
 const theme = createTheme({
-	palette: {
-		primary: {
-			main: cyan[800], // #00838F
-			dark: cyan[900], // #006064
-			contrastText: "#fff",
-		},
-		secondary: {
-			main: lightGreen[700], // #689F38
-			light: lightGreen["A400"], // #B2FF59
-			contrastText: "#fff",
-		},
-		grayText: "#656565",
-	},
+  palette: {
+    primary: {
+      main: cyan[800], // #00838F
+      dark: cyan[900], // #006064
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: lightGreen[700], // #689F38
+      light: lightGreen["A400"], // #B2FF59
+      contrastText: "#fff",
+    },
+    grayText: "#656565",
+  },
 });
 
 export default class App extends Component {
-	render() {
-		return (
-			// Componente ThemeProvider, aplica o tema personalizado
-			<ThemeProvider theme={theme}>
-				<Hero /> {/* Cabeçalho da pagina, onde fica o logo*/}
-				<Guests /> {/* Componente Guests, onde fica a lista de convidados */}
-				<Localização />{" "}
-				{/* Componente de Localização, onde fica a localização do evento */}
-			</ThemeProvider>
-		);
-	}
+  render() {
+    return (
+      // Componente ThemeProvider, aplica o tema personalizado
+      <ThemeProvider theme={theme}>
+        <Hero /> {/* Cabeçalho da pagina */}
+        <Guests /> {/* Seção Convidados */}
+        <Local /> {/* Seção Localização */}
+      </ThemeProvider>
+    );
+  }
 }
