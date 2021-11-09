f058dc4513dff33a8d70e02417396ad907fe0d2c
Victor Morais <moraisinho7@gmail.com>
2021-11-09 16:58:25 -0300

    ajustes no card localização

--- src/Localização/index.jsx
+++ src/Localização/index.jsx
@@ -7,6 +7,16 @@ import { ReactComponent as ImgLocalizacao } from "./img-localizacao.svg";
 const TextoLocalizacao = {
 	fontSize: "35px",
 	fontHeight: "bold",
+	paddingX: "3px",
+	paddingY: "3px",
+	margin: "15px",
+};
+
+const TituloLocalizacao = {
+	fontSize: "35px",
+	fontHeight: "bold",
+	paddingX: "3px",
+	paddingY: "3px",
 };
 
 export default function LocalizacaoCard() {
@@ -14,21 +24,18 @@ export default function LocalizacaoCard() {
 
 	return (
 		<Grid
+			className="grid-localizacao"
 			sx={{ backgroundColor: theme }}
-			x
-			container
 			spacing={1}
-			className="header"
 			alignItems="center"
 			paddingX={3}
 			paddingY={3}
 		>
-			<Grid item xs={12}>
+			<Grid xs={12}>
 				<Typography
-					style={TextoLocalizacao}
+					style={TituloLocalizacao}
 					variant="h6"
 					color="white"
-					className="texto-localizacao"
 					textAlign="center"
 				>
 					Localização
@@ -36,30 +43,27 @@ export default function LocalizacaoCard() {
 			</Grid>
 
 			{/* arrumar posicionamento da imagem */}
-			<Grid>
-				<ImgLocalizacao className="localizacao" />
+			<Grid className="localizacao">
+				<ImgLocalizacao />
 			</Grid>
-			{/* adicionando grid para ajustar elementos */}
-			<Grid xs={1}></Grid>
 
-			<Grid item xs={6}>
-				{/*TODO: Ajustar margins, paddings, espaçamento e imagem.
-            Bordas no mapa */}
+			{/*TODO: Ajustar margins, paddings, espaçamento e imagem.
+			 */}
 
-				<Typography style={TextoLocalizacao} color="white">
+			<Grid>
+				<Typography color="white">
 					Veja no mapa onde o evento será localizado. Diferente da anterior,
 					nesta edição a Feira do Livro voltará a ser realizada no Bourbon
 					Shopping.
 				</Typography>
 			</Grid>
-			<Grid>
+			<Grid className="mapa-localizacao">
 				<iframe
 					src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3514.470290394818!2d-52.3957468849259!3d-28.25375328256874!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x1d3b037fc8edaa67!2sBourbon%20Shopping%20Passo%20Fundo!5e0!3m2!1spt-BR!2sbr!4v1630606985214!5m2!1spt-BR!2sbr"
-					width="100%"
-					height="350"
-					allowfullscreen=""
+					width="552px"
+					height="350px"
 					loading="lazy"
-					border-radius="55px"
+					border-radius="8px"
 				></iframe>
 			</Grid>
 		</Grid>
