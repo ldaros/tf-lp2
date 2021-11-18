d19d7853676b0e9cc1467743c082b51ba157842d
ldaros <darosluan@gmail.com>
2021-11-18 18:24:17 -0300

    call to scroll

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import React, { Component } from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { cyan, lightGreen } from "@mui/material/colors"; // Cores do material-ui
 
-import Hero from "../Hero";
-import Guests from "../Guests";
-import Local from "../Local";
-import Discount from "../Discount";
-import Team from "../Team";
-import Form from "../Form";
-import Footer from "../Footer";
+import Hero from "./Hero";
+import Guests from "./Guests";
+import Local from "./Local";
+import Discount from "./Discount";
+import Team from "./Team";
+import Form from "./Form";
+import Footer from "./Footer";
+import Scroll from "./Scroll";
 
 //teste de commit 2
 // Tema personalizado
@@ -41,6 +42,7 @@ export default class App extends Component {
         <Team /> {/* Seção Equipe */}
         <Form /> {/* Formulário */}
         <Footer /> {/* Rodapé */}
+        <Scroll />
       </ThemeProvider>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Index do react, renderiza o componente principal
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./App.jsx";
 
 ReactDOM.render(
   <React.StrictMode>
