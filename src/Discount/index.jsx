edb8da594e14b0cfe3ba7224144ba64372433640
PedroAmarante0812 <86379291+PedroAmarante0812@users.noreply.github.com>
2021-11-18 02:22:04 -0300

    centralizando descontos

--- src/Discount/index.jsx
+++ src/Discount/index.jsx
@@ -8,34 +8,48 @@ export default function Discount() {
   const theme = useTheme();
 
   return (
-    <>
-      <Image
-        src={fotolivros}
-        sx={{ maxWidth: { xs: "60%", sm: "35%", md: "25%" } }}
-      ></Image>
-      <Typography
-        variant="h5"
-        align="center"
-        color={theme.palette.primary.dark}
-        sx={{ mb: { xs: 1, md: 3 }, fontSize: { md: "1.9em" } }}
-        mt={-1}
-      >
-        Se inscreva em nosso newsletter e ganhe 10% de desconto em qualquer
-        livro!
-      </Typography>
-      <Grid item xs={6}>
+    <Grid
+      container
+      spacing={2}
+      alignItems="center"
+      paddingX={{ xs: 3, sm: 8, md: 20, lg: 30 }}
+      paddingY={12}
+      position="relative"
+    >
+      <Grid Grid item xs={6} align="center">
+        <Image
+          src={fotolivros}
+          align="center"
+          sx={{ maxWidth: { xs: "80%", sm: "75%", md: "83%" } }}
+        ></Image>
+      </Grid>
+      <Grid Grid item xs={6}>
+        <Typography
+          variant="h5"
+          align="center"
+          color={theme.palette.primary.dark}
+          sx={{ mb: { xs: 1, md: 3 }, fontSize: { md: "1.9em" } }}
+          mt={-1}
+        >
+          Se inscreva em nosso newsletter e ganhe 10% de desconto em qualquer
+          livro!
+        </Typography>
+      </Grid>
+      <Grid item xs={12} alignSelf="center">
         <TextField
-          alignSelf="center"
           id="email"
-          label="email"
+          label="Email"
           type="email"
           variant="outlined"
           margin="normal"
+          fullWidth
         />
       </Grid>
-      <Button type="submit" variant="contained" color="primary">
-        Cadastrar
-      </Button>
-    </>
+      <Grid Grid item xs={12} align="center">
+        <Button type="submit" variant="contained" color="primary">
+          Inscrever-se
+        </Button>
+      </Grid>
+    </Grid>
   );
 }
