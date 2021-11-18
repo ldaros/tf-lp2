852a5c500988ff76f0767ac95120b8f517a8acaf
ldaros <darosluan@gmail.com>
2021-11-18 09:58:54 -0300

    Alterações no disconto

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -12,33 +12,33 @@ import Footer from "../Footer";
 
 // Tema personalizado
 const theme = createTheme({
-	palette: {
-		primary: {
-			main: cyan[800], // #00838F
-			dark: cyan[900], // #006064
-			contrastText: "#fff",
-		},
-		secondary: {
-			main: lightGreen[700], // #689F38
-			light: lightGreen["A400"], // #B2FF59
-			contrastText: "#fff",
-		},
-		grayText: "#656565",
-	},
+  palette: {
+    primary: {
+      main: cyan[800], // #00838F
+      dark: cyan[900], // #006064
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: lightGreen[700], // #689F38
+      light: lightGreen["A400"], // #B2FF59
+      contrastText: "#fff",
+    },
+    grayText: "#656565",
+  },
 });
 
 export default class App extends Component {
-	render() {
-		return (
-			// Componente ThemeProvider, aplica o tema personalizado
-			<ThemeProvider theme={theme}>
-				<Hero /> {/* Cabeçalho da pagina */}
-				<Guests /> {/* Seção Convidados */}
-				<Local /> {/* Seção Localização */}
-				<Discount /> {/* Seção Disconto */}
-				<Team /> {/* Seção Equipe */}
-				<Footer /> {/* Seção Rodapé */}
-			</ThemeProvider>
-		);
-	}
+  render() {
+    return (
+      // Componente ThemeProvider, aplica o tema personalizado
+      <ThemeProvider theme={theme}>
+        <Hero /> {/* Cabeçalho da pagina */}
+        <Guests /> {/* Seção Convidados */}
+        <Local /> {/* Seção Localização */}
+        <Discount /> {/* Seção Disconto */}
+        <Team /> {/* Seção Equipe */}
+        <Footer /> {/* Seção Rodapé */}
+      </ThemeProvider>
+    );
+  }
 }
